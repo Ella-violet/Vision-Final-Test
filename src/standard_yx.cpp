d666dd42fc9156e4966989ca7fa58a8b04ae8a92
SHM_white <1131287310a@gmail.com>
2026-03-29 15:57:50 +0800

    杨洵现场调试成功

--- src/standard_yx.cpp
+++ src/standard_yx.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include<chrono>
+#include<fmt/core.h>
+#include<fstream>
 #include<opencv2/opencv.hpp>
 
 #include<io/camera.hpp>
@@ -12,6 +14,7 @@
 #include<tasks/auto_aim/yolo.hpp>
 #include<tasks/auto_aim/solver.hpp>
 #include<tasks/auto_aim/tracker.hpp>
+#include<tasks/auto_aim/detector.hpp>
 #include<tools/exiter.hpp>
 #include<tools/img_tools.hpp>
 #include<tools/logger.hpp>
@@ -19,21 +22,20 @@
 #include<tools/plotter.hpp>
 #include<tools/recorder.hpp>
 
-const std::string keys:
-    "{help h usage?}"
-    "{@config-path | configs::test2.yaml}"
+const std::string keys=
+    "{@help h ? ||usage}"
+    "{@config-path | configs/test2.yaml|}";
 
 int main(int argc,char* argv[]) {
     cv::CommandLineParser cli(argc,argv,keys);
-    auto config_path = cli.get<std::string>(0);
-    if(cli.has("help") || config_path.empty()):
-    {
+    auto config_path = cli.get<std::string>(1);
+    if(cli.has("@help") || config_path.empty()){
         cli.printMessage();
         return 0;
     }
 
     tools::Exiter exiter;
-    tools::Ploter ploter;
+    tools::Plotter plotter;
     tools::Recorder recorder;
 
     io::Camera camera(config_path);
@@ -43,32 +45,34 @@ int main(int argc,char* argv[]) {
     auto_aim::Shooter shooter(config_path);
     auto_aim::Solver solver(config_path);
     auto_aim::Tracker tracker(config_path,solver);
-    auto_aim::YOLO detect(config_path,false);
+    auto_aim::YOLO detector(config_path,false);
 
     cv::Mat img;
     Eigen::Quaterniond q;
     std::chrono::steady_clock::time_point t;
     auto mode=io::GimbalMode::IDLE;
     auto last_mode=io::GimbalMode::IDLE;
+    auto state=io::GimbalState();
 
 
     while(!exiter.exit())
     {
         camera.read(img,t);
-        q=Eigen::Quaterniond imu_at(t);
-        mode=gimbal.GimbalMode;
+        q=gimbal.q(t);
+        mode=gimbal.mode();
+        state=gimbal.state();
         if(last_mode!=mode)
         {
-            tools::logger()->info("Switch to {}", io::MODES[mode]);
             last_mode=mode;
         }
         recorder.record(img,q,t);
         solver.set_R_gimbal2world(q);
-        Eigen::Vector3d ypr=tools::eulers(solver.R_gimbal2world(),0,1,2)
+        Eigen::Vector3d ypr=tools::eulers(solver.R_gimbal2world(),0,1,2);
         auto armors=detector.detect(img);
         auto target=tracker.track(armors,t);
-        auto command=aimer.aim(target,t,gimbal.state.bullet_spped);
-        gimbal.sent(command);
+        auto command=aimer.aim(target,t,state.bullet_speed);
+        command.shoot = false;
+        gimbal.send(command);
     }
 
     return 0;
